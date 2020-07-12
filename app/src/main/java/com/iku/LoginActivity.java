f9521f9a96de8c93c4dffb9d0583f9beddedc848
Abhishek AN <darkabhi1520@gmail.com>
2020-07-12 13:11:52 +0530

    Phone number login
    Signed-off-by: Abhishek AN <darkabhi1520@gmail.com>

--- app/src/main/java/com/iku/LoginActivity.java
+++ app/src/main/java/com/iku/LoginActivity.java
@@ -2,10 +2,13 @@ package com.iku;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -21,5 +24,32 @@ public class LoginActivity extends AppCompatActivity {
         numberInput = findViewById(R.id.phoneNumberInputText);
         signInButton = findViewById(R.id.signInButton);
 
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String mobile = numberInput.getText().toString().trim();
+                if (mobile.isEmpty() || mobile.length() < 10) {
+                    numberInput.setError("Enter a valid number");
+                    numberInput.requestFocus();
+                    return;
+                }
+                Intent intent = new Intent(LoginActivity.this, VerifyNumberActivity.class);
+                intent.putExtra("mobile", mobile);
+                startActivity(intent);
+            }
+        });
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (FirebaseAuth.getInstance().getCurrentUser() != null) {
+            Intent intent = new Intent(this, HomeActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+
+            startActivity(intent);
+        }
+    }
+
 }
