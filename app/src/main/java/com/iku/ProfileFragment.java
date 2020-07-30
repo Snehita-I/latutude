3375bf11457068a627104e1f9e10260f88df1484
Abhishek AN <darkabhi1520@gmail.com>
2020-07-31 00:19:50 +0530

    Update
    Signed-off-by: Abhishek AN <darkabhi1520@gmail.com>

--- app/src/main/java/com/iku/ProfileFragment.java
+++ app/src/main/java/com/iku/ProfileFragment.java
@@ -1,18 +1,31 @@
 package com.iku;
 
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.material.textview.MaterialTextView;
+
+import com.squareup.picasso.Picasso;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ProfileFragment extends Fragment {
+
+    private ImageView profilePicture;
+    private MaterialTextView userNameText;
+
+    private static final String TAG = ProfileFragment.class.getSimpleName();
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -23,6 +36,26 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        profilePicture = view.findViewById(R.id.profileImage);
+        userNameText = view.findViewById(R.id.userName);
+
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(getActivity().getApplicationContext());
+        if (account != null) {
+            String personName = account.getDisplayName();
+            String personGivenName = account.getGivenName();
+            String personFamilyName = account.getFamilyName();
+            String personEmail = account.getEmail();
+            String personID = account.getId();
+            Uri personPhoto = account.getPhotoUrl();
+
+            userNameText.setText(personName);
+            Picasso.with(getActivity()).load(personPhoto).into(profilePicture);
+
+            Log.e(TAG, "\nEmail: " + personEmail + "\nFamilyName: " + personFamilyName + "\nPhoto: " + personPhoto + "\nGiveName: " + personGivenName + "\nID: " + personID + "\nName: " + personName);
+
+        }
+        return view;
     }
 }
--- app/src/main/java/com/iku/WelcomeActivity.java
+++ app/src/main/java/com/iku/WelcomeActivity.java
@@ -10,6 +10,14 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
@@ -21,17 +29,27 @@ import com.google.android.gms.tasks.Task;
 import com.google.android.material.button.MaterialButton;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
 
 public class WelcomeActivity extends AppCompatActivity {
 
+    private CallbackManager mCallbackManager;
+    private LoginButton loginButton;
+
     private SignInButton googleSignInButton;
+
     private GoogleSignInClient mGoogleSignInClient;
-    public static final String TAG = WelcomeActivity.class.getSimpleName();
+
     private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private AccessTokenTracker accessTokenTracker;
+
     private MaterialButton emailButton;
+
+    public static final String TAG = WelcomeActivity.class.getSimpleName();
 
     private int RC_SIGN_IN = 121;
 
@@ -40,9 +58,15 @@ public class WelcomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
 
-
         googleSignInButton = findViewById(R.id.google_signin_button);
+
+        loginButton = findViewById(R.id.facebook_login_button);
+        loginButton.setReadPermissions("email","public_profile");
+        mCallbackManager = CallbackManager.Factory.create();
+
+
         mAuth = FirebaseAuth.getInstance();
+        FacebookSdk.sdkInitialize(getApplicationContext());
         emailButton = findViewById(R.id.email_signin_button);
 
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
@@ -59,10 +83,54 @@ public class WelcomeActivity extends AppCompatActivity {
             }
         });
 
+        loginButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.i("FB AUTH", "onSuccess: " + loginResult);
+                handleFacebookToken(loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel() {
+                Log.i(TAG, "onCancel: ");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.i(TAG, "onError: " + error);
+            }
+        });
+
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    String personName = user.getDisplayName();
+                    String personEmail = user.getEmail();
+                    String personPhoto = user.getPhotoUrl().toString();
+                    String personID = user.getUid();
+                    personPhoto = personPhoto + "?type=large";
+                    Log.e(TAG, "\nEmail: " + personEmail + "\nPhoto: " + personPhoto + "\nID: " + personID + "\nName: " + personName);
+
+                } else
+                    Log.i(TAG, "onAuthStateChanged: Not logged in");
+            }
+        };
+
+        accessTokenTracker = new AccessTokenTracker() {
+            @Override
+            protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
+                if (currentAccessToken == null) {
+                    mAuth.signOut();
+                }
+            }
+        };
+
         emailButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent enterEmailIntent = new Intent(WelcomeActivity.this,HomeActivity.class);
+                Intent enterEmailIntent = new Intent(WelcomeActivity.this, HomeActivity.class);
                 startActivity(enterEmailIntent);
             }
         });
@@ -76,6 +144,7 @@ public class WelcomeActivity extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == RC_SIGN_IN) {
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
@@ -101,16 +170,16 @@ public class WelcomeActivity extends AppCompatActivity {
                 if (task.isSuccessful()) {
                     Log.e(TAG, "onComplete: Successful");
                     FirebaseUser user = mAuth.getCurrentUser();
-                    updateUI(user);
+                    googleAccountDetails(user);
                 } else {
                     Log.e(TAG, "onComplete: failed");
-                    updateUI(null);
+                    googleAccountDetails(null);
                 }
             }
         });
     }
 
-    private void updateUI(FirebaseUser fUser) {
+    private void googleAccountDetails(FirebaseUser fUser) {
         GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(getApplicationContext());
         if (account != null) {
             String personName = account.getDisplayName();
@@ -125,4 +194,41 @@ public class WelcomeActivity extends AppCompatActivity {
         }
     }
 
+    private void handleFacebookToken(AccessToken token) {
+        Log.i(TAG, "handleFacebookToken: " + token);
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        mAuth.signInWithCredential(credential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Log.i(TAG, "SIGN IN WITH CREDENTIAL SUCCESS");
+                    FirebaseUser user = mAuth.getCurrentUser();
+                    if (user != null) {
+                        String personName = user.getDisplayName();
+                        String personEmail = user.getEmail();
+                        String personPhoto = user.getPhotoUrl().toString();
+                        String personID = user.getUid();
+                        personPhoto = personPhoto + "?type=large";
+                        Log.e(TAG, "\nEmail: " + personEmail + "\nPhoto: " + personPhoto + "\nID: " + personID + "\nName: " + personName);
+                    }
+                } else {
+                    Log.i(TAG, "SIGN IN WITH CREDENTIAL FAIL");
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(authStateListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (authStateListener != null) {
+            mAuth.removeAuthStateListener(authStateListener);
+        }
+    }
 }
