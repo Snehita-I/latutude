762fd28475d49eb6c169add92dccb96172c3590d
Renuka Pooja K G <pooja@printola.in>
2020-08-29 12:44:33 +0530

    Added imageUrl.java, threeimages.java, activity_settings.xml and activity_three_images.xml
    Signed-off-by: Abhishek AN <darkabhi1520@gmail.com>

---
+++ app/src/main/java/com/iku/threeImages.java
@@ -0,0 +1,466 @@
+package com.iku;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.webkit.MimeTypeMap;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.Timestamp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+public class threeImages extends AppCompatActivity {
+
+
+    private StorageReference mStorageRef;
+
+    ImageView d1, d2, d3;
+    EditText messageEntered;
+    Button upload;
+    int PICK_IMAGE = 1;
+
+    private FirebaseAuth mAuth;
+
+    private FirebaseUser user;
+
+    private FirebaseFirestore db;
+    private ImageView img1, img2, img3, img4;
+    String myArray[] = new String[3];
+   Uri UriArray[]= new Uri[3];
+    ImageButton s[]=new ImageButton[5];
+   private int counter=0;
+    List<Uri> myList = new ArrayList<>();
+    List<String> finalUrl = new ArrayList<>();
+
+    Uri mainUri;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_three_images);
+
+        for(int i=0;i<5;i++)
+        {
+            switch(i)
+            {
+                case 0:s[0]=findViewById(R.id.star1);
+                case 1:s[1]=findViewById(R.id.star2);
+                case 2:s[2]=findViewById(R.id.star3);
+                case 3:s[3]=findViewById(R.id.star4);
+                case 4:s[4]=findViewById(R.id.star5);
+            }
+        }
+
+
+
+        messageEntered = (EditText) findViewById(R.id.feedbackText);
+        upload = (Button) findViewById(R.id.submitButton);
+        img1 = (ImageView) findViewById(R.id.firstImage);
+        img2 = (ImageView) findViewById(R.id.secondImage);
+        img3 = (ImageView) findViewById(R.id.thirdImage);
+        img4 = (ImageView) findViewById(R.id.hiddenImageView);
+        d1 = (ImageView) findViewById(R.id.delete1);
+        d2 = (ImageView) findViewById(R.id.delete2);
+        d3 = (ImageView) findViewById(R.id.delete3);
+
+        db = FirebaseFirestore.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+        user = mAuth.getCurrentUser();
+
+        mStorageRef = FirebaseStorage.getInstance().getReference("feedback");
+
+        img1.setImageResource(R.drawable.addimages);
+        img2.setEnabled(false);
+        img3.setEnabled(false);
+        d1.setVisibility(View.INVISIBLE);
+        d2.setVisibility(View.INVISIBLE);
+        d3.setVisibility(View.INVISIBLE);
+
+        final ImageView[] imageBoxes = {img1, img2, img3,img4};
+        final ImageView[] deleteButtons = {d1,d2,d3};
+
+        img1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent();
+                i.setType("image/*");
+                i.setAction(Intent.ACTION_GET_CONTENT);
+
+                startActivityForResult(i, 0);
+            }
+        });
+
+        img2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent();
+                i.setType("image/*");
+                i.setAction(Intent.ACTION_GET_CONTENT);
+
+                startActivityForResult(i, 1);
+            }
+        });
+
+        img3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent();
+                i.setType("image/*");
+                i.setAction(Intent.ACTION_GET_CONTENT);
+
+                startActivityForResult(i, 2);
+            }
+        });
+
+        upload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+              uploadToStorage();
+            }
+        });
+
+
+        d1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                int k = 0, lastImage = 0;
+                myList.remove(k);
+                for (int i = k; i < imageBoxes.length - 1; i++) {
+                    if (imageBoxes[i + 1].getDrawable() != null) {
+
+                        imageBoxes[i].setImageDrawable(imageBoxes[i + 1].getDrawable());
+                        imageBoxes[i].setEnabled(false);
+                        deleteButtons[i].setVisibility(View.VISIBLE);
+                        deleteButtons[i].setEnabled(true);
+                        imageBoxes[i + 1].setImageDrawable(null);
+                        if(i<=1) {
+                            deleteButtons[i + 1].setVisibility(View.INVISIBLE);
+                            deleteButtons[i + 1].setEnabled(false);
+                        }
+                        lastImage = i;
+
+                    } else break;
+
+                }
+                imageBoxes[lastImage].setEnabled(true);
+                if(lastImage<=2) { deleteButtons[lastImage].setEnabled(false);
+                    deleteButtons[lastImage].setVisibility(View.INVISIBLE);
+                }
+
+            }
+        });
+        d2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                int k = 1, lastImage = 0;
+                myList.remove(k);
+                for (int i = k; i < imageBoxes.length - 1; i++) {
+                    if (imageBoxes[i + 1].getDrawable() != null) {
+
+                        imageBoxes[i].setImageDrawable(imageBoxes[i + 1].getDrawable());
+                        imageBoxes[i].setEnabled(false);
+                        deleteButtons[i].setVisibility(View.VISIBLE);
+                        deleteButtons[i].setEnabled(true);
+                        imageBoxes[i + 1].setImageDrawable(null);
+                        if(i<=1) {
+                            deleteButtons[i + 1].setVisibility(View.INVISIBLE);
+                            deleteButtons[i + 1].setEnabled(false);
+                        }
+                        lastImage = i;
+
+                    } else break;
+
+                }
+                imageBoxes[lastImage].setEnabled(true);
+                if(lastImage<=2) { deleteButtons[lastImage].setEnabled(false);
+                    deleteButtons[lastImage].setVisibility(View.INVISIBLE);
+                }
+
+            }
+        });
+        d3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                int k = 2, lastImage = 0;
+                myList.remove(k);
+                for (int i = k; i < imageBoxes.length - 1; i++) {
+                    if (imageBoxes[i + 1].getDrawable() != null) {
+
+                        imageBoxes[i].setImageDrawable(imageBoxes[i + 1].getDrawable());
+                        imageBoxes[i].setEnabled(false);
+                        deleteButtons[i].setVisibility(View.VISIBLE);
+                        deleteButtons[i].setEnabled(true);
+                        imageBoxes[i + 1].setImageDrawable(null);
+                        if(i<=1) {
+                            deleteButtons[i + 1].setVisibility(View.INVISIBLE);
+                            deleteButtons[i + 1].setEnabled(false);
+                        }
+                        lastImage = i;
+
+                    } else break;
+
+                }
+                imageBoxes[lastImage].setEnabled(true);
+                if(lastImage<=2) { deleteButtons[lastImage].setEnabled(false);
+                    deleteButtons[lastImage].setVisibility(View.INVISIBLE);
+                }
+
+            }
+        });
+
+
+    }
+
+    public void starfn(View view) {
+        if(view==s[0]) {
+            for(int i=0;i<=0;i++)
+            {
+                s[i].setImageResource(R.drawable.ic_filled_star);
+                Log.i("zero",Integer.toString(i)+"clicked");
+            }
+        }
+        else if(view==s[1]) {
+            for(int i=0;i<=1;i++)
+            {
+                s[i].setImageResource(R.drawable.ic_filled_star);
+                Log.i("one",Integer.toString(i)+"clicked");
+            }
+
+        }
+        else if(view==s[2]) {
+            for(int i=0;i<=2;i++)
+            {
+                s[i].setImageResource(R.drawable.ic_filled_star);
+                Log.i("two",Integer.toString(i)+"clicked");
+            }
+
+        }
+        else if(view==s[3]) {
+            for(int i=0;i<=3;i++)
+            {
+                s[i].setImageResource(R.drawable.ic_filled_star);
+                Log.i("three",Integer.toString(i)+"clicked");
+            }
+
+        }
+        else if(view==s[4]) {
+            for(int i=0;i<=4;i++)
+            {
+                s[i].setImageResource(R.drawable.ic_filled_star);
+                Log.i("four",Integer.toString(i)+"clicked");
+            }
+
+        }
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        ImageView[] imageBoxes = {img1, img2, img3,img4};
+        ImageView[] deleteButtons = {d1,d2,d3};
+
+        if (resultCode == RESULT_OK && data != null) {
+            Uri uri = data.getData();
+            myList.add(uri);
+            imageBoxes[requestCode].setImageDrawable(null);
+            imageBoxes[requestCode].setImageURI(uri);
+            deleteButtons[requestCode].setEnabled(true);
+            imageBoxes[requestCode].setEnabled(false);
+            deleteButtons[requestCode].setVisibility(View.VISIBLE);
+            if (requestCode <= 2) {
+                imageBoxes[requestCode + 1].setImageResource(R.drawable.addimages);
+                imageBoxes[requestCode + 1].setEnabled(true);
+                if(requestCode<=1) {
+                    deleteButtons[requestCode + 1].setEnabled(false);
+                    deleteButtons[requestCode+1].setVisibility(View.INVISIBLE);
+                }
+            }
+
+
+        } else
+            onBackPressed();
+    }
+
+
+    private String getFileExtension(Uri uri) {
+        if (uri != null) {
+            ContentResolver cR = getContentResolver();
+            MimeTypeMap mime = MimeTypeMap.getSingleton();
+            return mime.getExtensionFromMimeType(cR.getType(uri));
+        } else return null;
+    }
+
+    public static Bitmap decodeUri(Context c, Uri uri, final int requiredSize) throws FileNotFoundException {
+        BitmapFactory.Options o = new BitmapFactory.Options();
+        o.inJustDecodeBounds = true;
+        BitmapFactory.decodeStream(c.getContentResolver().openInputStream(uri), null, o);
+
+        int width_tmp = o.outWidth, height_tmp = o.outHeight;
+        int scale = 1;
+
+        while (true) {
+            if (width_tmp / 2 < requiredSize || height_tmp / 2 < requiredSize)
+                break;
+            width_tmp /= 2;
+            height_tmp /= 2;
+            scale *= 2;
+        }
+
+        BitmapFactory.Options o2 = new BitmapFactory.Options();
+        o2.inSampleSize = scale;
+        return BitmapFactory.decodeStream(c.getContentResolver().openInputStream(uri), null, o2);
+    }
+
+    public Uri getImageUri(Context inContext, Bitmap inImage) {
+
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+
+        String path = MediaStore.Images.Media.insertImage(inContext.getContentResolver(), inImage, "images", null);
+
+        if (path != null) return Uri.parse(path);
+        else return null;
+    }
+
+
+    private void uploadToStorage() {
+
+        for (Uri uri : myList)
+        {
+            try {
+                final Bitmap imageSelected = decodeUri(this, uri, 1080);
+               mainUri= getImageUri(threeImages.this, imageSelected);
+
+            final StorageReference imageRef = mStorageRef.child(System.currentTimeMillis() + "." + getFileExtension(mainUri));
+            UploadTask uploadTask = imageRef.putFile(uri);
+
+            uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Task<Uri> downloadUrl = imageRef.getDownloadUrl();
+                    downloadUrl.addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        @Override
+                        public void onSuccess(Uri uri) {
+                            counter++;
+
+                            finalUrl.add(uri.toString());
+
+                            Toast.makeText(threeImages.this, "uri added", Toast.LENGTH_LONG).show();
+                            if (counter == myList.size())
+                            {
+                                uploadToDB();
+                            }
+                        }
+                    });
+                }
+
+
+            });
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+
+    }
+    private void uploadToDB()
+    {
+
+        Map<String, Object> docData = new HashMap<>();
+        Map<String, List<String>> array = new HashMap<>();
+
+        imageUrl i = new imageUrl(finalUrl, user.getUid(),messageEntered.getText().toString());
+        docData.put("message", i);
+        docData.put("timestamp", new Timestamp(new Date()));
+        docData.put("to", "abhishek@printola.in");
+
+        db.collection("mail")
+                .add(docData)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        messageEntered.setText("");
+                        Toast.makeText(threeImages.this, "Image info uploaded", Toast.LENGTH_LONG).show();
+                         //SaveImage(imageSelected);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
+                        Toast.makeText(threeImages.this, e.toString(), Toast.LENGTH_LONG).show();
+                    }
+                });
+
+    }
+
+
+
+
+    private void SaveImage(Bitmap finalBitmap) {
+
+        String root = Environment.getExternalStorageDirectory().toString();
+        File myDir = new File(root + "/iku/images/sent_images");
+        if (!myDir.exists()) {
+            myDir.mkdirs();
+        }
+        Random generator = new Random();
+        int n = 10000;
+        n = generator.nextInt(n);
+        String fname = "Image-"+ n +".jpg";
+        File file = new File (myDir, fname);
+        if (file.exists ())
+            file.delete ();
+        try {
+            FileOutputStream out = new FileOutputStream(file);
+            finalBitmap.compress(Bitmap.CompressFormat.JPEG, 90, out);
+            out.flush();
+            out.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
