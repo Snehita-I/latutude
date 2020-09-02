743ba58bf6d10592084e2fd0168ea1888710223b
Abhishek AN <darkabhi1520@gmail.com>
2020-09-02 16:07:38 +0530

    Update Password Input flow
    Signed-off-by: Abhishek AN <darkabhi1520@gmail.com>

--- app/src/main/java/com/iku/PasswordInputActivity.java
+++ app/src/main/java/com/iku/PasswordInputActivity.java
@@ -3,6 +3,7 @@ package com.iku;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
@@ -16,6 +17,10 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.analytics.FirebaseAnalytics;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.iku.databinding.ActivityPasswordInputBinding;
 
 public class PasswordInputActivity extends AppCompatActivity {
@@ -24,9 +29,13 @@ public class PasswordInputActivity extends AppCompatActivity {
 
     private FirebaseAuth firebaseAuth;
 
+    private FirebaseUser user;
+
     private FirebaseAnalytics mFirebaseAnalytics;
 
     private ProgressDialog mProgress;
+    private static final String TAG = PasswordInputActivity.class.getSimpleName();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,18 +64,44 @@ public class PasswordInputActivity extends AppCompatActivity {
                 firebaseAuth.signInWithEmailAndPassword(enteredEmail, binding.enterPassword.getText().toString())
                         .addOnCompleteListener(PasswordInputActivity.this, task -> {
                             if (task.isSuccessful()) {
-                                Toast.makeText(PasswordInputActivity.this, "Login successful", Toast.LENGTH_LONG).show();
-                                //sending to Home Activity
-                                mProgress.dismiss();
-                                Intent goToHomeActivity = new Intent(PasswordInputActivity.this, HomeActivity.class);
-                                goToHomeActivity.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
-                                startActivity(goToHomeActivity);
+                                user = firebaseAuth.getCurrentUser();
+                                FirebaseFirestore rootRef = FirebaseFirestore.getInstance();
+                                DocumentReference docIdRef = rootRef.collection("users").document(user.getUid());
+                                docIdRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                        if (task.isSuccessful()) {
+                                            DocumentSnapshot document = task.getResult();
+                                            if (document.exists()) {
+                                                Log.d(TAG, "Document exists!");
+
+                                                Toast.makeText(PasswordInputActivity.this, "Login successful", Toast.LENGTH_LONG).show();
+                                                //sending to Home Activity
+                                                mProgress.dismiss();
+                                                Intent goToHomeActivity = new Intent(PasswordInputActivity.this, HomeActivity.class);
+                                                goToHomeActivity.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                                startActivity(goToHomeActivity);
 
 
-                                //log event
-                                Bundle password_bundle = new Bundle();
-                                password_bundle.putString(FirebaseAnalytics.Param.METHOD, "Email");
-                                mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.LOGIN, password_bundle);
+                                                //log event
+                                                Bundle password_bundle = new Bundle();
+                                                password_bundle.putString(FirebaseAnalytics.Param.METHOD, "Email");
+                                                mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.LOGIN, password_bundle);
+                                            } else {
+                                                Log.d(TAG, "Document does not exist!");
+                                                Toast.makeText(PasswordInputActivity.this, "User profile not filled!", Toast.LENGTH_LONG).show();
+                                                //sending to NameInput Activity
+                                                mProgress.dismiss();
+                                                Intent goToNameInputActivity = new Intent(PasswordInputActivity.this, NameInputActivity.class);
+                                                goToNameInputActivity.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                                startActivity(goToNameInputActivity);
+                                            }
+                                        } else {
+                                            Log.d(TAG, "Failed with: ", task.getException());
+                                        }
+                                    }
+                                });
+
 
                             } else {
                                 mProgress.dismiss();
