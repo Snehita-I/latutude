71d85970ce9cb5a08031c5e54ebf3a91cadd00f7
Abhishek AN <darkabhi1520@gmail.com>
2020-09-05 15:57:38 +0530

    Update
    Signed-off-by: Abhishek AN <darkabhi1520@gmail.com>

--- app/src/main/java/com/iku/UserProfileActivity.java
+++ app/src/main/java/com/iku/UserProfileActivity.java
@@ -68,7 +68,7 @@ public class UserProfileActivity extends AppCompatActivity {
                                 String firstLetter, secondLetter;
                                 String url = (String) document.get("imageURL");
                                 Log.d(TAG, "DocumentSnapshot data: " + document.getData() + document.get("imageURL"));
-                                if (url.isEmpty()) {
+                                if (url==null) {
                                     if (userName != null) {
                                         nameTextView.setText(userName);
 
