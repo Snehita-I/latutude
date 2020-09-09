c1362a1fe12b7847202980750a61fb61dfd56e6f
Abhishek AN <darkabhi1520@gmail.com>
2020-09-10 00:25:14 +0530

    Update WelcomeActivity.java
    Signed-off-by: Abhishek AN <darkabhi1520@gmail.com>

--- app/src/main/java/com/iku/WelcomeActivity.java
+++ app/src/main/java/com/iku/WelcomeActivity.java
@@ -50,7 +50,7 @@ public class WelcomeActivity extends AppCompatActivity {
 
     private CallbackManager mCallbackManager;
 
-    ActivityWelcomeBinding binding;
+    private ActivityWelcomeBinding binding;
 
     private GoogleSignInClient mGoogleSignInClient;
 
@@ -58,7 +58,6 @@ public class WelcomeActivity extends AppCompatActivity {
 
     private ProgressDialog mProgress;
 
-    private FirebaseFunctions mFunctions;
 
     private FirebaseFirestore db;
 
@@ -75,15 +74,6 @@ public class WelcomeActivity extends AppCompatActivity {
         binding = ActivityWelcomeBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
-
-        db = FirebaseFirestore.getInstance();
-        // Obtain the FirebaseAnalytics instance.
-        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
-
-        mAuth = FirebaseAuth.getInstance();
-        mFunctions = FirebaseFunctions.getInstance();
-
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestIdToken(getString(R.string.default_web_client_id))
                 .requestEmail()
@@ -92,7 +82,21 @@ public class WelcomeActivity extends AppCompatActivity {
 
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
 
+        // Initialize Facebook Login button
+        mCallbackManager = CallbackManager.Factory.create();
+        initItems();
         initProgressDialog();
+        initButtons();
+
+    }
+
+    private void initItems(){
+        db = FirebaseFirestore.getInstance();
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    private void initButtons() {
 
         binding.googleSigninButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -109,8 +113,6 @@ public class WelcomeActivity extends AppCompatActivity {
             }
         });
 
-        // Initialize Facebook Login button
-        mCallbackManager = CallbackManager.Factory.create();
         binding.facebookLoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -169,76 +171,7 @@ public class WelcomeActivity extends AppCompatActivity {
                                 }
                             }
                         } else {
-
                             String errorCode = ((FirebaseAuthException) task.getException()).getErrorCode();
-
-                            switch (errorCode) {
-
-                                case "ERROR_INVALID_CUSTOM_TOKEN":
-                                    Toast.makeText(WelcomeActivity.this, "The custom token format is incorrect. Please check the documentation.", Toast.LENGTH_LONG).show();
-                                    break;
-
-                                case "ERROR_CUSTOM_TOKEN_MISMATCH":
-                                    Toast.makeText(WelcomeActivity.this, "The custom token corresponds to a different audience.", Toast.LENGTH_LONG).show();
-                                    break;
-
-                                case "ERROR_INVALID_CREDENTIAL":
-                                    Toast.makeText(WelcomeActivity.this, "The supplied auth credential is malformed or has expired.", Toast.LENGTH_LONG).show();
-                                    break;
-
-                                case "ERROR_INVALID_EMAIL":
-                                    Toast.makeText(WelcomeActivity.this, "The email address is badly formatted.", Toast.LENGTH_LONG).show();
-
-                                    break;
-
-                                case "ERROR_WRONG_PASSWORD":
-                                    Toast.makeText(WelcomeActivity.this, "The password is invalid or the user does not have a password.", Toast.LENGTH_LONG).show();
-
-                                    break;
-
-                                case "ERROR_USER_MISMATCH":
-                                    Toast.makeText(WelcomeActivity.this, "The supplied credentials do not correspond to the previously signed in user.", Toast.LENGTH_LONG).show();
-                                    break;
-
-                                case "ERROR_REQUIRES_RECENT_LOGIN":
-                                    Toast.makeText(WelcomeActivity.this, "This operation is sensitive and requires recent authentication. Log in again before retrying this request.", Toast.LENGTH_LONG).show();
-                                    break;
-
-                                case "ERROR_ACCOUNT_EXISTS_WITH_DIFFERENT_CREDENTIAL":
-                                    Toast.makeText(WelcomeActivity.this, "An account already exists with the same email address but different sign-in credentials. Sign in using a provider associated with this email address.", Toast.LENGTH_LONG).show();
-                                    break;
-
-                                case "ERROR_EMAIL_ALREADY_IN_USE":
-                                    Toast.makeText(WelcomeActivity.this, "The email address is already in use by another account.   ", Toast.LENGTH_LONG).show();
-                                    break;
-
-                                case "ERROR_CREDENTIAL_ALREADY_IN_USE":
-                                    Toast.makeText(WelcomeActivity.this, "This credential is already associated with a different user account.", Toast.LENGTH_LONG).show();
-                                    break;
-
-                                case "ERROR_USER_DISABLED":
-                                    Toast.makeText(WelcomeActivity.this, "The user account has been disabled by an administrator.", Toast.LENGTH_LONG).show();
-                                    break;
-
-                                case "ERROR_USER_TOKEN_EXPIRED":
-
-                                case "ERROR_INVALID_USER_TOKEN":
-                                    Toast.makeText(WelcomeActivity.this, "The user\\'s credential is no longer valid. The user must sign in again.", Toast.LENGTH_LONG).show();
-                                    break;
-
-                                case "ERROR_USER_NOT_FOUND":
-                                    Toast.makeText(WelcomeActivity.this, "There is no user record corresponding to this identifier. The user may have been deleted.", Toast.LENGTH_LONG).show();
-                                    break;
-
-                                case "ERROR_OPERATION_NOT_ALLOWED":
-                                    Toast.makeText(WelcomeActivity.this, "This operation is not allowed. You must enable this service in the console.", Toast.LENGTH_LONG).show();
-                                    break;
-
-                                case "ERROR_WEAK_PASSWORD":
-                                    Toast.makeText(WelcomeActivity.this, "The given password is invalid.", Toast.LENGTH_LONG).show();
-                                    break;
-
-                            }
                         }
                     }
                 });
@@ -341,6 +274,10 @@ public class WelcomeActivity extends AppCompatActivity {
                                     if (task.isSuccessful()) {
                                         DocumentSnapshot document = task.getResult();
                                         if (document.exists()) {
+
+                                            FirebaseUser user = mAuth.getCurrentUser();
+                                            updateUI(user);
+
                                             //log event
                                             Bundle user_bundle = new Bundle();
                                             user_bundle.putString(FirebaseAnalytics.Param.METHOD, "Google/FB Sign In");
