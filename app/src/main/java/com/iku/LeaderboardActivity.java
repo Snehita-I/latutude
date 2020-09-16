f5d339a3241afdb2e7eb124e6565457c85727099
Abhishek AN <darkabhi1520@gmail.com>
2020-09-16 15:36:32 +0530

    Leaderboard complete
    Signed-off-by: Abhishek AN <darkabhi1520@gmail.com>

--- app/src/main/java/com/iku/LeaderboardActivity.java
+++ app/src/main/java/com/iku/LeaderboardActivity.java
@@ -1,42 +1,26 @@
 package com.iku;
 
-import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.os.CountDownTimer;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.paging.PagedList;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
-import com.firebase.ui.firestore.FirestoreRecyclerOptions;
-import com.firebase.ui.firestore.SnapshotParser;
 import com.firebase.ui.firestore.paging.FirestorePagingOptions;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.android.material.textview.MaterialTextView;
 import com.google.firebase.analytics.FirebaseAnalytics;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
-import com.iku.adapter.ChatAdapter;
 import com.iku.adapter.LeaderBoardAdapter;
-import com.iku.models.ChatModel;
 import com.iku.models.LeaderboardModel;
-import com.iku.utils.ItemClickSupport;
 
 import nl.dionsegijn.konfetti.KonfettiView;
 import nl.dionsegijn.konfetti.models.Shape;
@@ -77,19 +61,15 @@ public class LeaderboardActivity extends AppCompatActivity {
         Query query = firebaseFirestore.collection("users").orderBy("points", Query.Direction.DESCENDING);
 
         PagedList.Config config = new PagedList.Config.Builder()
-                .setInitialLoadSizeHint(10)
-                .setPageSize(5)
+                .setInitialLoadSizeHint(100)
+                .setPageSize(25)
                 .build();
         FirestorePagingOptions<LeaderboardModel> options = new FirestorePagingOptions.Builder<LeaderboardModel>()
-                .setQuery(query, config,  new SnapshotParser<LeaderboardModel>() {
-                    @NonNull
-                    @Override
-                    public LeaderboardModel parseSnapshot(@NonNull DocumentSnapshot snapshot) {
-                        LeaderboardModel leaderboardModel = snapshot.toObject(LeaderboardModel.class);
-                        String uid = snapshot.getId();
-                        leaderboardModel.setUid(uid);
-                        return leaderboardModel;
-                    }
+                .setQuery(query, config, snapshot -> {
+                    LeaderboardModel leaderboardModel = snapshot.toObject(LeaderboardModel.class);
+                    String uid = snapshot.getId();
+                    leaderboardModel.setUid(uid);
+                    return leaderboardModel;
                 })
                 .setLifecycleOwner(this)
                 .build();
@@ -102,70 +82,42 @@ public class LeaderboardActivity extends AppCompatActivity {
 
         firebaseFirestore.collection("users")
                 .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful()) {
-                            for (QueryDocumentSnapshot document : task.getResult()) {
-                                totalPlayers++;
-                                Long userPoints = (Long) document.get("points");
-                                if (userPoints != null) {
-                                    int i;
-                                    i = userPoints.intValue();
-                                    totalHearts += i;
-                                }
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        for (QueryDocumentSnapshot document : task.getResult()) {
+                            totalPlayers++;
+                            Long userPoints = (Long) document.get("points");
+                            if (userPoints != null) {
+                                int i;
+                                i = userPoints.intValue();
+                                totalHearts += i;
                             }
-                            heartscount.setText(Integer.toString(totalHearts));
-                            playerscount.setText(Integer.toString(totalPlayers));
-                        } else {
                         }
+                        heartscount.setText(Integer.toString(totalHearts));
+                        playerscount.setText(Integer.toString(totalPlayers));
+                    } else {
                     }
                 });
 
 
-        backButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                onBackPressed();
-            }
-        });
+        backButton.setOnClickListener(view -> onBackPressed());
 
-        leaderboardadapter.setOnItemClickListener((documentSnapshot, position) -> {
-            LeaderboardModel leaderboardModel = documentSnapshot.toObject(LeaderboardModel.class);
-            if(leaderboardModel.getUid().equals(user.getUid())){
-                final KonfettiView viewConfetti = findViewById(R.id.viewConfetti);
-                //Log event
-                Bundle easter_bundle = new Bundle();
-                easter_bundle.putString("easter_egg", "Leaderboard");
-                easter_bundle.putString("UID", leaderboardModel.getUid());
-                easter_bundle.putString("Name", leaderboardModel.getFirstName() + " " + leaderboardModel.getLastName());
-                mFirebaseAnalytics.logEvent("easter_egg_found", easter_bundle);
+        leaderboardadapter.setOnItemClickListener(() -> {
+            final KonfettiView viewConfetti = findViewById(R.id.viewConfetti);
 
-                Toast.makeText(LeaderboardActivity.this, "- drum roll -", Toast.LENGTH_SHORT).show();
-                viewConfetti.build()
-                        .addColors(Color.BLUE, Color.LTGRAY, getResources().getColor(R.color.colorPrimary), getResources().getColor(R.color.colorAccent))
-                        .setDirection(0.0, 359.0)
-                        .setSpeed(1f, 8f)
-                        .setFadeOutEnabled(true)
-                        .setTimeToLive(2000L)
-                        .addShapes(Shape.Square.INSTANCE, Shape.Circle.INSTANCE)
-                        .addSizes(new Size(10, 10f))
-                        .setPosition(-50f, viewConfetti.getWidth() + 50f, -50f, -50f)
-                        .streamFor(300, 5000L);
+            Toast.makeText(LeaderboardActivity.this, "- drum roll -", Toast.LENGTH_SHORT).show();
+            viewConfetti.build()
+                    .addColors(Color.BLUE, Color.LTGRAY, getResources().getColor(R.color.colorPrimary), getResources().getColor(R.color.colorAccent))
+                    .setDirection(0.0, 359.0)
+                    .setSpeed(1f, 8f)
+                    .setFadeOutEnabled(true)
+                    .setTimeToLive(2000L)
+                    .addShapes(Shape.Square.INSTANCE, Shape.Circle.INSTANCE)
+                    .addSizes(new Size(10, 10f))
+                    .setPosition(-50f, viewConfetti.getWidth() + 50f, -50f, -50f)
+                    .streamFor(300, 5000L);
 
-                new CountDownTimer(1 * 10000, 1000) {
-
-                    public void onTick(long millisUntilFinished) {
-                        firstNameTextView.setEnabled(false);
-                    }
-
-                    public void onFinish() {
-                        firstNameTextView.setEnabled(true);
-                    }
-                }.start();
-
-            }
         });
     }
-
 }
+
--- app/src/main/java/com/iku/adapter/LeaderBoardAdapter.java
+++ app/src/main/java/com/iku/adapter/LeaderBoardAdapter.java
@@ -1,40 +1,25 @@
 package com.iku.adapter;
 
-import android.content.Context;
 import android.graphics.Color;
-import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.NumberPicker;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
-import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.firebase.ui.firestore.paging.FirestorePagingAdapter;
 import com.firebase.ui.firestore.paging.FirestorePagingOptions;
-import com.firebase.ui.firestore.paging.LoadingState;
 import com.google.android.material.textview.MaterialTextView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.iku.LeaderboardActivity;
 import com.iku.R;
-import com.iku.models.ChatModel;
 import com.iku.models.LeaderboardModel;
 
-import java.text.SimpleDateFormat;
-
-import nl.dionsegijn.konfetti.KonfettiView;
-import nl.dionsegijn.konfetti.models.Shape;
-import nl.dionsegijn.konfetti.models.Size;
-
 public class LeaderBoardAdapter extends FirestorePagingAdapter<LeaderboardModel, RecyclerView.ViewHolder> {
+
     private FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
     private LeaderBoardAdapter.OnItemClickListener listener;
 
@@ -47,32 +32,9 @@ public class LeaderBoardAdapter extends FirestorePagingAdapter<LeaderboardModel,
         LeaderBoardAdapter.LeaderboardViewHolder leaderboardViewHolder = (LeaderboardViewHolder) viewHolder;
         leaderboardViewHolder.firstNameTextView.setText(leaderboardModel.getFirstName() + " " + leaderboardModel.getLastName());
         leaderboardViewHolder.pointsTextView.setText(String.valueOf(leaderboardModel.getPoints()));
-
-        if(leaderboardModel.getUid().equals(user.getUid())){
+        if (leaderboardModel.getUid().equals(user.getUid())) {
             leaderboardViewHolder.firstNameTextView.setTextColor(Color.parseColor("#11b7b7"));
             leaderboardViewHolder.pointsTextView.setTextColor(Color.parseColor("#11b7b7"));
-        }
-    }
-
-    @Override
-    protected void onLoadingStateChanged(@NonNull LoadingState state){
-        super.onLoadingStateChanged(state);
-        switch (state){
-            case LOADED:
-                Log.d("leaderboard", "Total members loaded " + getItemCount());
-                break;
-            case ERROR:
-                Log.d("leaderboard", "Errored out ");
-                break;
-            case FINISHED:
-                Log.d("leaderboard", "Completed loading ");
-                break;
-            case LOADING_MORE:
-                Log.d("leaderboard", "More items loaded ");
-                break;
-            case LOADING_INITIAL:
-                Log.d("leaderboard", "Initial items loaded ");
-                break;
         }
     }
 
@@ -83,53 +45,38 @@ public class LeaderBoardAdapter extends FirestorePagingAdapter<LeaderboardModel,
         return new LeaderBoardAdapter.LeaderboardViewHolder(view);
     }
 
-    public class LeaderboardViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+    public class LeaderboardViewHolder extends RecyclerView.ViewHolder {
 
-        public MaterialTextView firstNameTextView,pointsTextView;
+        public MaterialTextView firstNameTextView, pointsTextView;
 
         public LeaderboardViewHolder(@NonNull View itemView) {
             super(itemView);
             firstNameTextView = itemView.findViewById(R.id.firstname);
             pointsTextView = itemView.findViewById(R.id.pointsText);
-            itemView.setOnClickListener(this);
-        }
-
-        @Override
-        public void onClick(View v) {
-            LeaderboardModel leaderboardModel = getItem(getAdapterPosition()).toObject(LeaderboardModel.class);
-            if(leaderboardModel.getUid().equals(user.getUid())){
-                Log.i("TEST", "onClick => Current user : " + leaderboardModel.getUid());
-            }
+            itemView.setOnClickListener(view -> {
+                LeaderboardModel leaderboardModel = getItem(getAdapterPosition()).toObject(LeaderboardModel.class);
+                if (leaderboardModel.getUid().equals(user.getUid())) {
+                    Log.i("TEST", "onClick => Current user : " + leaderboardModel.getUid());
+                    listener.onItemClick();
+                    new CountDownTimer(10000, 1000) {
+                        public void onTick(long millisUntilFinished) {
+                            itemView.setEnabled(false);
+                        }
+                        public void onFinish() {
+                            itemView.setEnabled(true);
+                        }
+                    }.start();
+                }
+            });
         }
     }
 
-
-
-
-//    public class LeaderboardViewHolder extends RecyclerView.ViewHolder{
-//        public MaterialTextView firstNameTextView,pointsTextView;
-//
-//        public LeaderboardViewHolder(@NonNull View itemView) {
-//            super(itemView);
-//            firstNameTextView = itemView.findViewById(R.id.firstname);
-//            pointsTextView = itemView.findViewById(R.id.pointsText);
-//            itemView.setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View view) {
-////                    int position = getAdapterPosition();
-////                    if (position != RecyclerView.NO_POSITION && listener != null) {
-////                        listener.onItemClick(getSnapshots().getSnapshot(position), position);
-////                    }
-//                }
-//            });
-//        }
-//    }
     public void setOnItemClickListener(LeaderBoardAdapter.OnItemClickListener listener) {
         this.listener = listener;
     }
 
     public interface OnItemClickListener {
-        void onItemClick(DocumentSnapshot documentSnapshot, int position);
+        void onItemClick();
     }
 }
 
